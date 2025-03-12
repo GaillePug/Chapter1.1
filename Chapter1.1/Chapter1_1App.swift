ae39cb3db27fd0b07c40f1b455a92861e6c6a8f8
hello <hello@home>
2025-03-12 11:27:49 +0800

    Initial Commit

---
+++ Chapter1.1/Chapter1_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter1_1App.swift
+//  Chapter1.1
+//
+//  Created by Teresa Gaille Teo on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Chapter1_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
