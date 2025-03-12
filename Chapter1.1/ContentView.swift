7c2c54ff0217275b6b5e710f1775b2a0a4be616e
hello <hello@home>
2025-03-12 12:29:49 +0800

    Updates after first lesson

--- Chapter1.1/ContentView.swift
+++ Chapter1.1/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "crown")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.pink)
+            Text("Come as you are")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.pink)
         }
         .padding()
     }
